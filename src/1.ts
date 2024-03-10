12a2ff5a1d33aa69f0413f3a2b68cb8b941fdcbe
Olena Voina <105532756+olli20@users.noreply.github.com>
2024-03-10 11:52:23 +0100

    in progress

--- src/1.ts
+++ src/1.ts
@@ -4,16 +4,18 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+interface IStudent {
+  name: string;
+  age: number;
+  grade: string;
 }
 
-export {};
+class Student implements IStudent {
+  constructor(
+    public name: string,
+    public age: number,
+    public grade: string
+  ) { }
+}
+
+export {Student};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+    private signature: number;
 
-const house = new MyHouse(key);
-const person = new Person(key);
+    constructor() {
+      this.signature = Math.floor(Math.random() * 100); //Returns a random integer from 0 to 99
+    }
+  
+    getSignature(this: Key): number {
+      return this.signature;
+    }
+  }
 
-house.openDoor(person.getKey());
+  class MyHouse {
+    private savedKey: number;
+    protected isDoorOpen: boolean;
+    protected isPersonAtHome: boolean;
+      
+    constructor(key: Key) {
+      this.savedKey = key.getSignature();
+      this.isDoorOpen = false;
+      this.isPersonAtHome = false;
+    }
+      
+    openDoor(insertedKey: number): void {
+      if (insertedKey === this.savedKey) {
+        this.isDoorOpen = true;
+        console.log("Key is " + insertedKey + ". The door is open");
+      }
+    }
 
-house.comeIn(person);
+    comeIn(name: string) {
+      if (this.isDoorOpen) {
+        this.isPersonAtHome = true;
+        console.log(name + " is at home")
+      }
+    }
+  }
 
+  class Person {
+    private savedKey: number;
+    protected personsName: string;
+    constructor(key: Key, name: string) {
+      this.savedKey = key.getSignature()
+      this.personsName = name;
+    }
+
+    getKey(): number {
+      return this.savedKey;
+    }
+    getName(): string {
+      return this.personsName;
+    }
+  }
+
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key, "Max");
+
+  house.openDoor(person.getKey());
+  house.comeIn(person.getName());
 
 export {};
